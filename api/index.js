00f658bcd70aa92b6208dbc95c670d8716870a08
zapchicken <zapchicken@yahoo.com>
2025-08-17 15:38:12 -0300

    Fix: Configuração Vercel - Criado api/index.js para rotas

---
+++ api/index.js
@@ -0,0 +1,7 @@
+// 🚀 Arquivo específico para Vercel
+// Este arquivo é necessário para o Vercel reconhecer as rotas
+
+const app = require('../src/web.js');
+
+// Exporta o app para o Vercel
+module.exports = app;
